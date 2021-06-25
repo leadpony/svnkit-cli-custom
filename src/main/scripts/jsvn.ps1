b180d3e3cd1e40f4f40c3bcc657b98a305092515
leadpony <hacker@leadpony.org>
2021-06-25 15:28:03 +0900

    Add PowerShell script to the dist

---
+++ src/main/scripts/jsvn.ps1
@@ -0,0 +1,25 @@
+﻿$commandDir = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $commandDir -Parent
+$baseDir = Split-Path $scriptDir -Parent
+
+$javacmd = $env:JAVACMD
+if ($null -eq $javacmd) {
+    $javacmd = "java"
+    if ($null -ne $env:JAVA_HOME) {
+        $javacmd = "$env:JAVA_HOME\bin\java"
+    }
+}
+
+$libDir = $env:SVNKIT_LIB
+if ($null -eq $libDir) {
+    $libDir = "$baseDir\lib"
+}
+
+$classpath = "$libDir\*"
+$jvmargs=@(
+    "-Djava.util.logging.config.file=$baseDir\conf\logging.properties",
+    "-Dsun.io.useCanonCaches=false")
+
+$allargs = $jvmargs + @('-classpath', $classpath, 'org.tmatesoft.svn.cli.SVN') + $Args
+
+Start-Process -FilePath $javacmd -ArgumentList $allargs -Wait -NoNewWindow
